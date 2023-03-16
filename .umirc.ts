3bced6413113aaac4352b183e2e9c9b0e57f65d9
jr0349 <jr0349@magima.com>
2023-03-16 16:27:58 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'dumi';
+
+let base = '/sarajiang-ui';
+let publicPath = '/sarajiang-ui/';
+
+if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
+  base = undefined;
+  publicPath = undefined;
+}
+export default defineConfig({
+  title: 'SARAJIANG UI',
+  mode: 'site',
+  outputPath: 'doc-site',
+  exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
+  dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
+  webpack5: {},
+});
